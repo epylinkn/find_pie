2ed814d2065e31db344f11080e12ee8e620a0f76
ant <anthonybui@gmail.com>
2012-06-12 15:16:58 -1000

    switch db to postgresql

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 FindPie::Application.routes.draw do
   root :to => "static_pages#index"
-  get "static_pages/info"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
