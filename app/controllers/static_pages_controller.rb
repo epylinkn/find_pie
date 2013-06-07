bc7014d984c779ea448095d485637f8d5d055440
ant <anthonybui@gmail.com>
2013-06-06 23:54:29 -0700

    :boom: DELETED ENTIRE RAILS APP :boom:
    but keeping history as i change this project/repo to use middleman on heroku instead of rails (static > dynamic, right?)

--- app/controllers/static_pages_controller.rb
+++
@@ -1,7 +0,0 @@
-class StaticPagesController < ApplicationController
-  def index
-  end
-
-  def info
-  end
-end
--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-FindPie::Application.config.secret_token = '9fda1efeecd4cc3a93ab4c9961973439def17e37182c767bf95adb1146f0fb9b6970fd421aeefb27305ac3eb889c4fb6752d0525069d8bd09474e1e5731cb146'
